50c45c175859491265ff7f86d2dc96e611a73013
Ananda Muhammad Muthaqin <anandamuhammadmtq@gmail.com>
2022-07-17 15:54:14 +0800

    initial

---
+++ pages/about.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+          hendrerit efficitur ligula posuere pharetra. Ut id sollicitudin augue.
+          Etiam pharetra mollis metus ut tincidunt. Nunc tincidunt tincidunt
+          dolor, quis mollis odio pretium et. Orci varius natoque penatibus et
+          magnis dis parturient montes, nascetur ridiculus mus. Nullam vel sem
+          aliquet, condimentum mauris ut, posuere ligula. Vivamus eleifend, diam
+          efficitur porta suscipit, ex diam auctor leo, a auctor eros libero sit
+          amet dui.
+        </p>
+        <p>
+          Suspendisse porttitor non elit eu congue. Praesent eu felis euismod
+          est eleifend bibendum id sed lacus. Suspendisse nunc odio, fermentum
+          ac augue non, dignissim posuere nibh. Proin non volutpat orci.
+          Suspendisse eleifend ex at erat ultricies, quis blandit leo accumsan.
+          Nunc dictum sapien mauris, non bibendum diam gravida id. Curabitur
+          mollis, augue id malesuada venenatis, nisl tortor eleifend tellus, nec
+          convallis quam velit non ipsum.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
